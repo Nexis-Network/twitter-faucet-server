b7e2f7e8c143872fc9c20e24f16bda4885cce1b8
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-06-25 04:26:42 +0530

    fetching tweets

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from ntscraper import Nitter
 
 app = Flask(__name__)
+scraper = Nitter()
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Nexis Twitter Faucet v1'
+
+@app.route('/search/<value>')
+def search(value):
+    tweets = scraper.get_tweets(str(value),mode='user',number=5)
+    print(tweets)
+    return f'You searched for: {value}'
 
 if __name__ == '__main__':
     app.run(debug=True)
